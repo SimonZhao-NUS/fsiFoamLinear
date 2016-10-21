c940cbfe4a95af8ff416c5ef1c2ea429f157185d
szhao <simon.xiang.zhao@gmail.com>
2016-10-21 15:52:08 +0800

    minor modification

--- fsiFoamLinear.C
+++ fsiFoamLinear.C
@@ -22,12 +22,10 @@ License
     along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
 
 Application
-    rhoCentralDyMFoam
+    fsiFoamLinear
 
 Description
-    Density-based compressible flow solver based on central-upwind schemes of
-    Kurganov and Tadmor
-
+    Roe based fvm solver. Dynamic linearization implemented.
 \*---------------------------------------------------------------------------*/
 
 #include "fvCFD.H"
@@ -49,129 +47,17 @@ int main(int argc, char *argv[])
     #include "createFields.H"
     #include "createTimeControls.H"
     #include "readTimeControls.H"
-    #include "readFarFieldValue.H"
+//    #include "readFarFieldValue.H"
 
 
     Info<< "\nStarting time loop\n" << endl;
 
-    //////bd corr//////////
-    thermo.correct();
-    volScalarField rPsi(1.0/psi);
-    volScalarField c(sqrt(thermo.gamma()*rPsi));
-    e = rhoE/rho - 0.5*magSqr(U);
-    p = rho*e*(thermo.gamma()-1);
-    U.dimensionedInternalField() =
-	rhoU.dimensionedInternalField()
-       /rho.dimensionedInternalField();
-    farFieldBoundaryCondition(rho, U, p, c, U_inf, rho_inf, p_inf);
-    T = p/rho/(thermo.gamma()-1)/thermo.Cv();
-    thermo.correct();
-    rhoU.boundaryField() = rho.boundaryField()*U.boundaryField();
-    rhoE.boundaryField() =
-	rho.boundaryField()*
-	(
-	    e.boundaryField() + 0.5*magSqr(U.boundaryField())
-	);
-    //////bd corr//////
 
     while ( runTime.run() || (ifLinear == "Yes"))
     {
-	//p boundary condition
-        e = rhoE/rho - 0.5*magSqr(U);
-        p = rho*e*(thermo.gamma()-1);
-/*        p.dimensionedInternalField() =
-            rho.dimensionedInternalField()
-           /psi.dimensionedInternalField();*/
-//        p.correctBoundaryConditions();
-	//rho boundary condition ***
-//	rho.boundaryField() = psi.boundaryField()*p.boundaryField();
-	//U boundary condition
-	U.dimensionedInternalField() =
-            rhoU.dimensionedInternalField()
-           /rho.dimensionedInternalField();
-//        U.correctBoundaryConditions();
-	farFieldBoundaryCondition(rho, U, p, c, U_inf, rho_inf, p_inf);
+	//reconstruct conservative flow variables on cell interfaces
+	#include "conVarRecon.H"
 
-	//e boundary condition ***
-//        e.correctBoundaryConditions();
-//        e.boundaryField() = p.boundaryField()/rho.boundaryField()/(thermo.gamma()().boundaryField()-1);
-//	T.boundaryField() = p.boundaryField()/rho.boundaryField()/(thermo.gamma()().boundaryField()-1)/thermo.Cv()().boundaryField();
-	T = p/rho/(thermo.gamma()-1)/thermo.Cv();
-        thermo.correct();
-        //
-        rhoU.boundaryField() = rho.boundaryField()*U.boundaryField();
-        rhoE.boundaryField() =
-            rho.boundaryField()*
-            (
-                e.boundaryField() + 0.5*magSqr(U.boundaryField())
-            );
-/////////boundary condtition end ////////////////
-
-        // --- upwind interpolation of primitive fields on faces
-        surfaceScalarField gamma
-        (
-	    "gamma",
-            fvc::interpolate(thermo.gamma(), "reconstruct(gamma)")
-        );
-        surfaceScalarField rho_pos
-        (
-	    "rho_pos",
-            myInterpolatePos(rho)
-        );
-
-        surfaceScalarField rho_neg
-        (
-	    "rho_neg",
-            myInterpolateNeg(rho)
-        );
-     
-        surfaceVectorField rhoU_pos
-        (
-	    "rhoU_pos",
-            myInterpolatePos(rhoU)
-        );
-        surfaceVectorField rhoU_neg
-        (
-	    "rhoU_neg",
-            myInterpolateNeg(rhoU)
-        );
-
-        surfaceScalarField rhoE_pos
-        (
-	    "rhoE_pos",
-            myInterpolatePos(rhoE)
-        );
-        surfaceScalarField rhoE_neg
-        (
-	    "rhoE_neg",
-            myInterpolateNeg(rhoE)
-        );
-
-/*	myMat matQ;
-	for(unsigned int ni = 0; ni < mesh.nCells(); ni++) {
-	    matQ.push_back(std::vector<double>(1,rho.internalField()[ni]));
-	    matQ.push_back(std::vector<double>(1,rhoU.internalField()[ni].x()));
-	    matQ.push_back(std::vector<double>(1,rhoU.internalField()[ni].y()));
-	    matQ.push_back(std::vector<double>(1,rhoU.internalField()[ni].z()));
-	    matQ.push_back(std::vector<double>(1,rhoE.internalField()[ni]));
-	}
-	myMatWrite(matQ, "matQ");
-	
-	SpMat matQf_pos(mesh.owner().size()*5, 1);
-	SpMat matQf_neg(mesh.owner().size()*5, 1);
-	TripletList QfList_pos, QfList_neg;
-	for (int fi=0; fi<mesh.owner().size(); fi++) {
-	    QfList_pos.push_back(Triplet(fi*5+0, 0, rho_pos[fi]));
-	    QfList_neg.push_back(Triplet(fi*5+0, 0, rho_neg[fi]));
-	}
-	matQf_pos.setFromTriplets(QfList_pos.begin(), QfList_pos.end());
-	matQf_neg.setFromTriplets(QfList_neg.begin(), QfList_neg.end());
-
-	myMatWriteDenSp(matQf_pos, "matQf_pos");	
-	myMatWriteDenSp(matQf_neg, "matQf_neg");
-	Info << "Qf" << endl;
-	cin.get();*/
-	//
         //
 	volScalarField rPsi(1.0/psi);
 	volScalarField c(sqrt(thermo.gamma()*rPsi));
@@ -277,35 +163,37 @@ int main(int argc, char *argv[])
 	// --- Solve density          
         solve(fvm::ddt(rho) + fvc::div(phi));
 
-    // --- Solve momentum
+	// --- Solve momentum
         solve(fvm::ddt(rhoU) + fvc::div(phiUp));
-      
-      // --- Solve energy
-	solve(fvm::ddt(rhoE) + fvc::div(phiEp));
-
-	//p boundary condition
-        e = rhoE/rho - 0.5*magSqr(U);
-        p = rho*e*(thermo.gamma()-1);
-//        p.correctBoundaryConditions();
-	//rho boundary condition ***
-//	rho.boundaryField() = psi.boundaryField()*p.boundaryField();
-	//U boundary condition
-	U.dimensionedInternalField() =
+        U.dimensionedInternalField() =
             rhoU.dimensionedInternalField()
            /rho.dimensionedInternalField();
-//        U.correctBoundaryConditions();
-	farFieldBoundaryCondition(rho, U, p, c, U_inf, rho_inf, p_inf);
-
-	//e boundary condition ***
-	T = p/rho/(thermo.gamma()-1)/thermo.Cv();
+        U.correctBoundaryConditions();
+        rhoU.boundaryField() == rho.boundaryField()*U.boundaryField();
+      
+        // --- Solve energy
+	solve(fvm::ddt(rhoE) + fvc::div(phiEp));
+        e = rhoE/rho - 0.5*magSqr(U);
+        e.correctBoundaryConditions();
         thermo.correct();
-        //
-        rhoU.boundaryField() = rho.boundaryField()*U.boundaryField();
-        rhoE.boundaryField() =
+        rhoE.boundaryField() ==
             rho.boundaryField()*
             (
                 e.boundaryField() + 0.5*magSqr(U.boundaryField())
             );
+        p.dimensionedInternalField() =
+            rho.dimensionedInternalField()
+           /psi.dimensionedInternalField();
+        p.correctBoundaryConditions();
+        rho.boundaryField() == psi.boundaryField()*p.boundaryField();
+
+        turbulence->correct();
+
+	// update values
+	// correct boundary conditions
+	// correct conservative variables
+	// correct intermediate variables
+
 
         runTime.write();
 
@@ -321,20 +209,59 @@ int main(int argc, char *argv[])
 }
 
 // ************************************************************************* //
-//
-//
-/*	if (ifLinear == "Yes") {
-	    #include "linearizationSp.H"
-	}
+/*
+    //////bd corr//////////
+    thermo.correct();
+    volScalarField rPsi(1.0/psi);
+    volScalarField c(sqrt(thermo.gamma()*rPsi));
+    e = rhoE/rho - 0.5*magSqr(U);
+    p = rho*e*(thermo.gamma()-1);
+    U.dimensionedInternalField() =
+	rhoU.dimensionedInternalField()
+       /rho.dimensionedInternalField();
+    farFieldBoundaryCondition(rho, U, p, c, U_inf, rho_inf, p_inf);
+    T = p/rho/(thermo.gamma()-1)/thermo.Cv();
+    thermo.correct();
+    rhoU.boundaryField() = rho.boundaryField()*U.boundaryField();
+    rhoE.boundaryField() =
+	rho.boundaryField()*
+	(
+	    e.boundaryField() + 0.5*magSqr(U.boundaryField())
+	);
+    //////bd corr//////
 
-	// --- Solve density          
-        solve(fvm::ddt(rho) + fvc::div(phi));
 
-    // --- Solve momentum
-        solve(fvm::ddt(rhoU) + fvc::div(phiUp));
-      
-      // --- Solve energy
-	solve(fvm::ddt(rhoE) + fvc::div(phiEp));
+	//p boundary condition
+        e = rhoE/rho - 0.5*magSqr(U);
+        p = rho*e*(thermo.gamma()-1);
+//        p.dimensionedInternalField() =
+//            rho.dimensionedInternalField()
+//           /psi.dimensionedInternalField();
+//        p.correctBoundaryConditions();
+	//rho boundary condition ***
+//	rho.boundaryField() = psi.boundaryField()*p.boundaryField();
+	//U boundary condition
+	U.dimensionedInternalField() =
+            rhoU.dimensionedInternalField()
+           /rho.dimensionedInternalField();
+//        U.correctBoundaryConditions();
+	farFieldBoundaryCondition(rho, U, p, c, U_inf, rho_inf, p_inf);
+
+	//e boundary condition ***
+//        e.correctBoundaryConditions();
+//        e.boundaryField() = p.boundaryField()/rho.boundaryField()/(thermo.gamma()().boundaryField()-1);
+//	T.boundaryField() = p.boundaryField()/rho.boundaryField()/(thermo.gamma()().boundaryField()-1)/thermo.Cv()().boundaryField();
+	T = p/rho/(thermo.gamma()-1)/thermo.Cv();
+        thermo.correct();
+        //
+        rhoU.boundaryField() = rho.boundaryField()*U.boundaryField();
+        rhoE.boundaryField() =
+            rho.boundaryField()*
+            (
+                e.boundaryField() + 0.5*magSqr(U.boundaryField())
+            );
+/////////boundary condtition end ////////////////
+	
 
 	//p boundary condition
         e = rhoE/rho - 0.5*magSqr(U);
@@ -359,26 +286,4 @@ int main(int argc, char *argv[])
             (
                 e.boundaryField() + 0.5*magSqr(U.boundaryField())
             );
-
-        runTime.write();
-	*/
-
-
-/*    forAll(mesh.boundary(), patchi) {
-	if (mesh.boundary()[patchi].name() != "inlet" &&
-	    mesh.boundary()[patchi].name() != "outlet"
-	) {
-	    for (int n = 0; n < mesh.boundary()[patchi].size(); n++) {
-		int fn = mesh.boundary()[patchi].start() + n;
-		forAll (mesh.faces()[fn], p) {
-		    int pn = mesh.faces()[fn][p];
-		    scalar Lx = mesh.points()[pn].x() - .25;
-		    scalar Ly = mesh.points()[pn].y();
-		    scalar dy = Lx*alpha;
-		    scalar dx = -Ly*alpha;
-		    newPoints[pn].x() = mesh.points()[pn].x()+dx;
-		    newPoints[pn].y() = mesh.points()[pn].y()+dy;
-		}
-	    }
-	}
-    }*/
+*/
